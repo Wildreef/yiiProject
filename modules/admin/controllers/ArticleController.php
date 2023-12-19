39b151c91aa7be108979b426f17342667011a407
Wildreef <125145548+Wildreef@users.noreply.github.com>
2023-12-19 10:16:36 +0200

    Work with images / now you can add images

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -2,17 +2,43 @@
 
 namespace app\modules\admin\controllers;
 
+use Yii;
 use app\models\Article;
 use app\models\ArticleSearch;
+use app\models\ImageUpload;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
 
 /**
  * ArticleController implements the CRUD actions for Article model.
  */
 class ArticleController extends Controller
 {
+
+    public function actionSetImage($id)
+    {
+        $model = new ImageUpload;
+
+        if(Yii::$app->request->isPost){
+
+            $article = $this->findModel($id);
+
+            $file = UploadedFile::getInstance($model, 'image');
+
+            if($article->saveImage($model->uploadFile($file, $article->image))){
+                return $this->redirect(['view', 'id'=>$article->id]);
+            }
+            /*
+            $article->saveImage($model->uploadFile($file));
+
+            $model->uploadFile($file);
+            */
+        }
+
+        return $this->render('image', ['model' => $model]);
+    }
     /**
      * @inheritDoc
      */
