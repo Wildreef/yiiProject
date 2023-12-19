600f9cbb65541a2c1a4bb224059f995bb6af2106
Makss464 <125145548+wildreef@users.noreply.github.com>
2023-12-19 11:46:31 +0200

    updated main page

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -60,24 +60,43 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 
 <main id="main" class="flex-shrink-0" role="main">
     <div class="container">
-        <?php if (!empty($this->params['breadcrumbs'])): ?>
-            <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
-        <?php endif ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</main>
 
-<footer id="footer" class="mt-auto py-3 bg-light">
-    <div class="container">
-        <div class="row text-muted">
-            <div class="col-md-6 text-center text-md-start">&copy; My Company <?= date('Y') ?></div>
-            <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
+        <!--main content start-->
+
+        <div class="main-content">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <?= $content ?>
+
+                </div>
+
+            </div>
+
         </div>
+
     </div>
-</footer>
+
+    </div>
+
+    <footer class="footer">
+
+        <div class="container">
+
+            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+
+            <p class="pull-right"><?= Yii::powered() ?></p>
+
+        </div>
+
+    </footer>
+
+    <?php $this->endBody() ?>
 
 <?php $this->endBody() ?>
+</main>
 </body>
 </html>
 <?php $this->endPage() ?>
